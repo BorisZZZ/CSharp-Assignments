5108b480cc1eec1e3a3790f602571014cf8fe3a8
Boris Zikeyev <Boris.Zikeyev@gmail.com>
2012-12-16 00:46:38 +0200

    Initial Project

---
+++ A13 Ex03 Boris 306301763 Moran 037200474/Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Vehicle
+    {
+        public abstract string ModelName { get; }
+        public abstract string LisenceNUmber { get; }
+        public abstract float FuelLevel { get; set; }
+        public Wheels CarWheels { get; }
+
+        public abstract class Wheels
+        {
+            abstract public byte WHeelsCount { get; }
+
+            abstract public Wheel this[byte i_Index] { get; set; }
+        }
+    }
+}
---
+++ A13 Ex03 Boris 306301763 Moran 037200474/Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Wheel
+    {
+        public string Manufacturer
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public float MaximumAirPressure
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public float AirPressure
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool PumpAir(float i_AddedAir)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
